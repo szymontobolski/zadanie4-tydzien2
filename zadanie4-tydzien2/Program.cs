06dc70d7fa9c8005c8dc527631933ff5a1e75acc
Szymon <Szymon@LAPTOP-2DIS65OF>
2021-09-18 16:36:10 +0200

    Add project files.

---
+++ zadanie4-tydzien2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zadanie4_tydzien2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 4, tydzień 2, lekcja 4, Szymon Tobolski");
+
+            byte a = 10;
+            string b = "Szkoła Dotneta";
+            Double c = 12.5;
+
+
+        }
+    }
+}
